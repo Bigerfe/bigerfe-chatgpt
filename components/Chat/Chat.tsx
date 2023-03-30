c19bf5b220d7c19104b8315e1f562d12894c30ca
bigerfe <223344386@qq.com>
2023-03-30 22:52:05 +0800

    add

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -67,11 +67,10 @@ export const Chat: FC<Props> = ({ conversation, models, apiKey, messageIsStreami
     <div className="relative flex-1 overflow-none dark:bg-[#343541] bg-white">
       {!apiKey && (
         <div className="flex flex-col justify-center mx-auto h-full w-[300px] sm:w-[500px] space-y-6">
-          <div className="text-1xl font-semibold text-center text-gray-400 dark:text-gray-100">Hi,我是程序员饭哥，叫我小饭就行。<br/>爱好互联网、编程、AI领域，做过后端、前端，写过C/C++、Java、Android、PHP，现在做前端领域。</div>
+          <div className="text-1xl font-semibold text-center text-gray-400">Hi,我是程序员饭哥,爱好互联网、编程、AI领域，做过后端、前端，写过C/C++、Java、Android、PHP，现在做前端领域。</div>
           {/* <div className="text-1xl font-semibold text-center text-gray-800 dark:text-gray-100">在这里都能找到我的痕迹，我的公众号-前端技术江湖、<a target="_blank" href="http://bigerfe.com">大前端面试刷题网</a>、<a href="https://github.com/Bigerfe/koa-react-ssr" target="_blank">React SSR开发框架</a>、gpt小程序版：小饭talk（近期上线） </div> */}
-          <div className="text-1xl font-semibold text-center text-gray-400 dark:text-gray-100">我的愿景：带领一小部分人走在前面，紧跟时代，把握机遇。</div>
-          <div className="text-2xl font-semibold text-center text-gray-800 dark:text-gray-100">需要OpenAI API Key 才能对话。 </div>
-          <div className="text-2xl font-semibold text-center text-gray-800 dark:text-gray-100">请先在页面左下方设置您的OpenAI API key。 </div>
+          <div className="text-1xl font-semibold text-center text-gray-400">我的愿景：带领一小部分人走在前面，紧跟时代，把握机遇。</div>
+          <div className="text-2xl font-semibold text-center text-gray-800 dark:text-gray-100">需要OpenAI API Key 才能对话。 <a style={{color:'green', textDecoration:'underline'}} href="https://mp.weixin.qq.com/s/jAuF5D9eQ75bsIBqQa8k0Q">key设置教程</a> </div>
           <div className="text-2xl font-semibold text-center text-gray-800 dark:text-gray-100">如果没有key，请关注作者公众号，直接获取共享KEY。 </div>
           <div className="text-center text-gray-500 dark:text-gray-800 dark:text-gray-100"></div>
           {/* <div className="text-center text-gray-500 dark:text-gray-400">也可以进入作者的知识星球『玩转chatGPT和AI绘画』，目前加入星球可赠送独立账号1个+KEY+N个共享账号，<a style={{color:'#fff'}} target="_blank" href="https://t.zsxq.com/0cl2u1Qem">点我打开介绍~</a></div> */}
