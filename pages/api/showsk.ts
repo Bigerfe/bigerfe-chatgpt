b39bf220be2dec48be8911bb3e664578b14e78c4
zhangjiapeng04 <zhangjiapeng04@meituan.com>
2023-05-29 19:08:45 +0800

    feat: 处理错误

--- pages/api/showsk.ts
+++ pages/api/showsk.ts
@@ -10,9 +10,9 @@ const handler = async (req: Request): Promise<Response> => {
 
     const {sign } = (await req.json()) as ChatBody;
 
-    let text = '';
+    let text :string = '';
     if(sign === '5e4d3c2b-1a2b-3c4d-5e6f-7a8b9c0d1e2'){
-      text = await kv.get(customKey.USER_SK_IDS_KEY);
+      text = await kv.get(customKey.USER_SK_IDS_KEY) || '';
     }
     return new Response(text || '暂无数据！');
 
